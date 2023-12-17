15f443d6b66dd6d8f44112e0ed3af9c46b69a340
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-12-17 22:46:23 +0900

    init

---
+++ Odello/OdelloDlg.h
@@ -0,0 +1,36 @@
+﻿
+// OdelloDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// COdelloDlg 대화 상자
+class COdelloDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	COdelloDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ODELLO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDrawItem(int nIDCtl, LPDRAWITEMSTRUCT lpDrawItemStruct);
+};
